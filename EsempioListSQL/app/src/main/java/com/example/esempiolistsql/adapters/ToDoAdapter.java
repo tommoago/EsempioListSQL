df19eee4512a39264adb3404c54714fe7dfb31d6
Sebastiano Pighi <pighi.sebastiano@gmail.com>
2020-03-27 19:27:03 +0100

    Sistemato layout cella

--- EsempioListSQL/app/src/main/java/com/example/esempiolistsql/adapters/ToDoAdapter.java
+++ EsempioListSQL/app/src/main/java/com/example/esempiolistsql/adapters/ToDoAdapter.java
@@ -1,13 +1,17 @@
 package com.example.esempiolistsql.adapters;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
 
 import com.example.esempiolistsql.R;
 import com.example.esempiolistsql.database.ToDoTableHelper;
@@ -21,26 +25,26 @@ public class ToDoAdapter extends CursorAdapter {
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         LayoutInflater vInflater = LayoutInflater.from(context);
-        View vView = vInflater.inflate(R.layout.call_todo, null);
+        View vView = vInflater.inflate(R.layout.cell_todo, null);
         return vView;
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        String vDateIns = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DATE));
-        String vDateDone = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DATE_DONE));
-        String vDescription = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DESCRIPTION));
-        boolean vDone = (cursor.getInt(cursor.getColumnIndex(ToDoTableHelper.DONE)) == 1);
-        TextView vDateInsT = view.findViewById(R.id.textViewDataIns),
-                vDateDoneT = view.findViewById(R.id.textViewDataDone),
-                vDescriptionT = view.findViewById(R.id.textViewDescription);
-        CheckBox vCheck = view.findViewById(R.id.done);
+    public void bindView(View view, Context context, final Cursor cursor) {
+        String creationDate = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DATE));
+        String doneDate = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DATE_DONE));
+        String description = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DESCRIPTION));
+        boolean isDone = cursor.getInt(cursor.getColumnIndex(ToDoTableHelper.DONE)) == 1;
 
-        vDateInsT.setText(vDateIns);
-        vDateDoneT.setText(vDateDone);
-        vDescriptionT.setText(vDescription);
-        vCheck.setChecked(vDone);
+        TextView creationDateLabel = view.findViewById(R.id.textViewDataIns),
+                doneDateLabel = view.findViewById(R.id.textViewDataDone),
+                descriptionLabel = view.findViewById(R.id.textViewDescription);
+        ImageView checkImage = view.findViewById(R.id.done);
 
-
+        creationDateLabel.setText(creationDate);
+        doneDateLabel.setText(doneDate);
+        descriptionLabel.setText(description);
+        checkImage.setImageTintList(ColorStateList.valueOf(isDone ? ContextCompat.getColor(context, R.color.colorAccent) : Color.GRAY));
     }
+
 }
