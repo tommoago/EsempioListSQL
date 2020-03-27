724f7d78a760a05e39cac671de8acb9fd4c8ff4c
tommo.ago <tommo.ago@gmail.com>
2020-03-27 11:47:42 +0100

    logica activity

--- EsempioListSQL/app/src/main/java/com/example/esempiolistsql/MainActivity.java
+++ EsempioListSQL/app/src/main/java/com/example/esempiolistsql/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.esempiolistsql;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView mList;
+    Button mButtonNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mList = findViewById(R.id.listViewToDo);
+        mButtonNew = findViewById(R.id.buttonNew);
     }
 }
