59881c296f1796ae64dd3d85435f4feb52c439e7
tommo.ago <tommo.ago@gmail.com>
2020-03-27 12:19:38 +0100

    logica adapter

--- EsempioListSQL/app/src/main/java/com/example/esempiolistsql/ToDoAdapter.java
+++ EsempioListSQL/app/src/main/java/com/example/esempiolistsql/ToDoAdapter.java
@@ -2,9 +2,12 @@ package com.example.esempiolistsql;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 public class ToDoAdapter extends CursorAdapter {
 
@@ -14,11 +17,27 @@ public class ToDoAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        LayoutInflater vInflater = LayoutInflater.from(context);
+        View vView = vInflater.inflate(R.layout.call_todo, null);
+        return vView;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        String vDateIns = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DATE));
+        String vDateDone = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DATE_DONE));
+        String vDescription = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DESCRIPTION));
+        boolean vDone = (cursor.getInt(cursor.getColumnIndex(ToDoTableHelper.DONE)) == 1);
+        TextView vDateInsT = view.findViewById(R.id.textViewDataIns),
+                vDateDoneT = view.findViewById(R.id.textViewDataDone),
+                vDescriptionT = view.findViewById(R.id.textViewDescription);
+        CheckBox vCheck = view.findViewById(R.id.done);
+
+        vDateInsT.setText(vDateIns);
+        vDateDoneT.setText(vDateDone);
+        vDescriptionT.setText(vDescription);
+        vCheck.setChecked(vDone);
+
 
     }
 }
