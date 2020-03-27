756b927961364beea0c6fa16a5b391444ab15ce6
tommo.ago <tommo.ago@gmail.com>
2020-03-27 12:26:30 +0100

    mostro elementi in lista

--- EsempioListSQL/app/src/main/java/com/example/esempiolistsql/MainActivity.java
+++ EsempioListSQL/app/src/main/java/com/example/esempiolistsql/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.esempiolistsql;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
@@ -17,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mList = findViewById(R.id.listViewToDo);
         mButtonNew = findViewById(R.id.buttonNew);
+
+        Cursor vElements = new ToDoDB(this).getReadableDatabase().query(ToDoTableHelper.TABLE_NAME,
+                null, null, null, null, null, null);
+
+        ToDoAdapter vAdapter = new ToDoAdapter(this, vElements);
+
+        mList.setAdapter(vAdapter);
     }
 }
