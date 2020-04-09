ea4a0c5526d64c0227be507c67a171f4a3a0eb71
tommo.ago <tommo.ago@gmail.com>
2020-04-09 11:10:36 +0200

    adapter user

--- EsempioListSQL/app/src/main/java/com/example/esempiolistsql/adapters/ToDoAdapter.java
+++ EsempioListSQL/app/src/main/java/com/example/esempiolistsql/adapters/ToDoAdapter.java
@@ -3,7 +3,9 @@ package com.example.esempiolistsql.adapters;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +16,9 @@ import android.widget.TextView;
 import androidx.core.content.ContextCompat;
 
 import com.example.esempiolistsql.R;
+import com.example.esempiolistsql.database.ToDoDB;
 import com.example.esempiolistsql.database.ToDoTableHelper;
+import com.example.esempiolistsql.database.UserTableHelper;
 
 public class ToDoAdapter extends CursorAdapter {
 
@@ -35,12 +39,24 @@ public class ToDoAdapter extends CursorAdapter {
         String doneDate = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DATE_DONE));
         String description = cursor.getString(cursor.getColumnIndex(ToDoTableHelper.DESCRIPTION));
         boolean isDone = cursor.getInt(cursor.getColumnIndex(ToDoTableHelper.DONE)) == 1;
+        int id = cursor.getInt(cursor.getColumnIndex(ToDoTableHelper.ID_USER));
+        Log.d("asda", "bindView: "+id);
+
+        String username = "";
+
+        SQLiteDatabase db = new ToDoDB(context).getReadableDatabase();
+        Cursor datas = db.query(UserTableHelper.TABLE_NAME, new String[]{UserTableHelper.USERNAME, UserTableHelper._ID}, UserTableHelper._ID + " = " + id,
+                null, null, null, null);
+        datas.moveToNext();
+        username = datas.getString(datas.getColumnIndex(UserTableHelper.USERNAME));
 
         TextView creationDateLabel = view.findViewById(R.id.textViewDataIns),
                 doneDateLabel = view.findViewById(R.id.textViewDataDone),
-                descriptionLabel = view.findViewById(R.id.textViewDescription);
+                descriptionLabel = view.findViewById(R.id.textViewDescription),
+                usernameLabel = view.findViewById(R.id.textViewUsername);
         ImageView checkImage = view.findViewById(R.id.done);
 
+        usernameLabel.setText(username);
         creationDateLabel.setText(creationDate);
         doneDateLabel.setText(doneDate);
         descriptionLabel.setText(description);
