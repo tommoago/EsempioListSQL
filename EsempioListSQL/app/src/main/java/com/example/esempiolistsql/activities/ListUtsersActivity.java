f76991241761298e949406d20a1b126451c300f5
tommo.ago <tommo.ago@gmail.com>
2020-04-09 11:58:32 +0200

    activity lista users

---
+++ EsempioListSQL/app/src/main/java/com/example/esempiolistsql/activities/ListUtsersActivity.java
@@ -0,0 +1,77 @@
+package com.example.esempiolistsql.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.esempiolistsql.R;
+import com.example.esempiolistsql.adapters.UserAdapter;
+import com.example.esempiolistsql.database.ToDoDB;
+import com.example.esempiolistsql.database.UserTableHelper;
+
+public class ListUtsersActivity extends AppCompatActivity {
+
+    final String tableName = UserTableHelper.TABLE_NAME;
+    final String sortOrder = UserTableHelper.SURNAME + " ASC ";
+
+
+    ListView list;
+    UserAdapter adapter;
+    ToDoDB toDoDB;
+    SQLiteDatabase db;
+    Button newUser;
+    Cursor users;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_users);
+        list = findViewById(R.id.listUsers);
+        toDoDB = new ToDoDB(this);
+        newUser = findViewById(R.id.buttonNewUser);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        db = toDoDB.getReadableDatabase();
+        if (db != null) {
+            loadUsers();
+        } else {
+            //TODO show an empty view inside ListView
+            newUser.setEnabled(false);
+            Toast.makeText(this, R.string.database_error, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void loadUsers() {
+        users = db.query(UserTableHelper.TABLE_NAME, null, null, null,
+                null, null, sortOrder);
+        if (users != null) {
+            if (adapter == null) {
+                adapter = new UserAdapter(this, users);
+                list.setAdapter(adapter);
+            } else {
+                adapter.changeCursor(users);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (users != null)
+            users.close();
+        if (db != null)
+            db.close();
+    }
+
+}
