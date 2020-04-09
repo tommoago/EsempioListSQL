642e21f946454ef1256ba79a1a8f84f68a4cb4c9
tommo.ago <tommo.ago@gmail.com>
2020-04-09 10:33:29 +0200

    aggiunto user id a todotablehelper

--- EsempioListSQL/app/src/main/java/com/example/esempiolistsql/database/ToDoTableHelper.java
+++ EsempioListSQL/app/src/main/java/com/example/esempiolistsql/database/ToDoTableHelper.java
@@ -9,11 +9,13 @@ public class ToDoTableHelper implements BaseColumns {
     public static final String DONE = "done";
     public static final String DATE = "date";
     public static final String DATE_DONE = "datedone";
+    public static final String ID_USER = "id_user";
 
     public static final String CREATE = "CREATE TABLE " + TABLE_NAME + " ( " +
             _ID + " INTEGER PRIMARY KEY AUTOINCREMENT , " +
             DESCRIPTION + " TEXT , " +
             DONE + " INTEGER DEFAULT 0 , " +
             DATE + " TEXT , " +
+            ID_USER + " INTEGER , " +
             DATE_DONE + " TEXT ) ;";
 }
